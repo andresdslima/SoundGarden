c0018aef40c9d686dce2179903c85fde83fd6e13
Andre Lima <andresdslima@gmail.com>
2022-03-28 20:23:15 +0200

    uploading my-modal

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // MODAL
+const open = document.querySelectorAll('.open');
+const close = document.querySelector('#close');
+const modalContainer = document.querySelector('.modal-container');
+
+open.forEach(element => {
+    element.addEventListener('click', () => {
+        modalContainer.classList.add('show');
+    });
+});
+
+close.addEventListener('click', () => {
+    modalContainer.classList.remove('show');
+});
